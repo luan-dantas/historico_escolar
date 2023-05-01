922e410ab8c334277b3c28b87689ae767490502f
Luan Dantas <dantasluan2405@gmail.com>
2023-05-01 04:27:14 -0300

    adicionando estrutura de dados com informações sobre os aluno

--- server.js
+++ server.js
@@ -2,6 +2,29 @@ const express = require('express');
 
 const app = express();
 
+// Estrutura de dados para armazenar as informações do aluno e seu histórico escolar
+const alunos = [
+    {
+      nome: 'John Doe',
+      matricula: '12345',
+      historico: [
+        { disciplina: 'Matemática', media: 8.5 },
+        { disciplina: 'Português', media: 9.0 },
+        { disciplina: 'História', media: 7.0 }
+      ]
+    },
+    {
+      nome: 'Jane Doe',
+      matricula: '67890',
+      historico: [
+        { disciplina: 'Matemática', media: 7.0 },
+        { disciplina: 'Português', media: 9.5 },
+        { disciplina: 'História', media: 6.5 }
+      ]
+    }
+  ];
+
+
 // iniciar o servidor
 app.listen(3000, () =>{
     console.log('Servidor rodando na porta 3000');
